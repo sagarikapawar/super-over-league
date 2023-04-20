cf10bcccde59dbea0c0599e99a9ad04769d3e595
sagarikapawar <sagarikapawar.698@gmail.com>
2023-04-20 10:30:27 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div class="container">
+    <h1>Super Over League</h1>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        class="img"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        class="img"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
